cc0bca94fb7004db75a7b7eb9fe6f8b303384e68
Caroline <carol-html@hotmail.com>
2025-01-15 14:12:33 -0300

    Update Magic.java
    Use of compound assignment operators to modify variable values ​​more efficiently.

--- principalprogram/Magic.java
+++ principalprogram/Magic.java
@@ -2,14 +2,14 @@ package principalprogram;
 
 public class Magic {
     public static void main(String[] args) {
-        int myNumber = 30;
-        int stepOne = myNumber * myNumber;
-        int stepTwo = stepOne + myNumber;
-        int stepThree = stepTwo / myNumber;
-        int stepFour = stepThree + 17;
-        int stepFive = stepFour - myNumber;
-        int stepSix = stepFive / 6;
+        int myNumber = 100;
+        int magicNumber = myNumber * myNumber;
+        magicNumber += myNumber;
+        magicNumber /= myNumber;
+        magicNumber += 17;
+        magicNumber -= myNumber;
+        magicNumber /= 6;
 
-        System.out.println(stepSix);
+        System.out.println(magicNumber);
     }
 }
